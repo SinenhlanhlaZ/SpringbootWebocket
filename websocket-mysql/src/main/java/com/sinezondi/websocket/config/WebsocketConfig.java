b4364079d5a02dd8765c9188f90e4df97f69379a
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2025-01-26 06:50:58 +0200

    resolving parameter error

--- websocket-mysql/src/main/java/com/sinezondi/websocket/config/WebsocketConfig.java
+++ websocket-mysql/src/main/java/com/sinezondi/websocket/config/WebsocketConfig.java
@@ -2,6 +2,7 @@ package com.sinezondi.websocket.config;
 
 import com.sinezondi.websocket.core.service.StockService;
 import com.sinezondi.websocket.handler.WebSocketHandler;
+import com.sinezondi.websocket.util.ObjectMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -15,14 +16,17 @@ public class WebsocketConfig implements WebSocketConfigurer {
 
     private final StockService stockService;
 
+    private final ObjectMap map;
+
     @Autowired
-    public WebsocketConfig(StockService stockService){
+    public WebsocketConfig(StockService stockService, ObjectMap map){
         this.stockService = stockService;
+        this.map = map;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-        registry.addHandler(new WebSocketHandler(stockService), "/ws")
+        registry.addHandler(new WebSocketHandler(stockService, map), "/ws")
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .setAllowedOrigins("*");
     }
